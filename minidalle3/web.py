596d5fe4c7b652cd5c81018f159464af219c4492
Zeqiang Lai <laizeqiang@outlook.com>
2023-10-07 22:35:12 +0800

    traffic control for demo

--- minidalle3/web.py
+++ minidalle3/web.py
@@ -1,7 +1,10 @@
 import gradio as gr
 import logging
 import fire
-import pandas
+import uuid
+import os
+import datetime
+import json
 
 from .model import MiniDALLE3
 
@@ -13,6 +16,12 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__file__)
+SAVE_DIR = 'saved'
+
+user_quota = {}
+if os.path.exists('user_quota.json'):
+    user_quota = json.load(open('user_quota.json', 'r'))
+MAX_QUOTA_PER_USER = 30
 
 
 def user(content):
@@ -28,14 +37,31 @@ def add_text(history, text):
     return history, gr.update(value="", interactive=False)
 
 
-def bot(history, state):
+def bot(history, state, allowed, request: gr.Request):
+    current_datetime = datetime.datetime.now()
+    current_date_as_string = current_datetime.strftime("%Y-%m-%d")
+    save_dir = os.path.join(SAVE_DIR, request.client.host, current_date_as_string, state['id'])
+    os.makedirs(save_dir, exist_ok=True)
+    
+    # track quota
+    if current_date_as_string not in user_quota:
+        user_quota[current_date_as_string] = {}
+    if request.client.host not in user_quota[current_date_as_string]:
+        user_quota[current_date_as_string][request.client.host] = MAX_QUOTA_PER_USER
+    quota = user_quota[current_date_as_string][request.client.host]
+    if quota <= 0:
+        history[-1][1] = f'Sorry, you have reach the limit of request per day ({MAX_QUOTA_PER_USER}).'
+        return history, state
+                    
     state['messages'].append(user(history[-1][0]))
     raw_output, response = state['model'].ask(state['messages'], state['images'])
     state['messages'].append(ai(raw_output))
 
     if response is not None:
-        image_filename = 'image.png'
+        image_filename = f'{uuid.uuid4()}.png'
+        image_filename = os.path.join(save_dir, 'img', image_filename)
         image = response.image
+        os.makedirs(os.path.join(save_dir, 'img'), exist_ok=True)
         image.save(image_filename)
         state['images'].append(image)
         state['image_prompts'].append(response.image_prompt)
@@ -44,6 +70,11 @@ def bot(history, state):
     else:
         history[-1][1] = raw_output
 
+    if allowed:
+        json.dump(history, open(os.path.join(save_dir, 'chat.json'), 'w'), indent=4)
+        
+    user_quota[current_date_as_string][request.client.host] -= 1
+    json.dump(user_quota, open('user_quota.json', 'w'), indent=4)
     return history, state
 
 
@@ -51,7 +82,7 @@ def on_select_image(evt: gr.SelectData, state):
     return state['image_prompts'][evt.index]
 
 
-def main(llm='gpt3.5', port=10049, prompt_path=None):
+def main(llm='gpt3.5', port=10049, prompt_path=None, max_users=2, share=False):
     model = MiniDALLE3(llm, prompt_path=prompt_path)
 
     with gr.Blocks() as demo:
@@ -61,11 +92,13 @@ def main(llm='gpt3.5', port=10049, prompt_path=None):
             <p align="center"> Mini-DALLE3: Interactive Text to Image Generation by Prompting Large Language Models. </p>
             """,
         )
-
+       
         state = gr.State({'messages': [model.system_message],
                           'images': [],
                           'image_prompts': [],
-                          'model': model})
+                          'model': model,
+                          'id': str(uuid.uuid4())
+                          })
 
         with gr.Tab('Mini DALLE3'):
             chatbot = gr.Chatbot(
@@ -87,18 +120,7 @@ def main(llm='gpt3.5', port=10049, prompt_path=None):
             )
             btn = gr.Button('Submit', variant='primary', min_width=50, scale=1)
 
-        txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
-            bot, [chatbot, state], [chatbot, state]
-        )
-        txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
-
-        btn.click(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
-            bot, [chatbot, state], [chatbot, state]
-        ).then(lambda: gr.update(interactive=True), None, [txt], queue=False).then(
-            lambda state: state['images'], [state], [gallery]
-        )
-        gallery.select(on_select_image, [state], [prompt])
-
+        allowed = gr.Checkbox(value=True, label='Will you allow us to save the chat history for future development?')
         gr.Examples(
             ['I have read a story where it talks about an "astronaut riding a horse" -- What does it look like ?',
              'Can I see more like this ?',
@@ -108,8 +130,20 @@ def main(llm='gpt3.5', port=10049, prompt_path=None):
             inputs=[txt],
             label='Examples',
         )
+        
+        txt_msg = txt.submit(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
+            bot, [chatbot, state, allowed], [chatbot, state]
+        )
+        txt_msg.then(lambda: gr.update(interactive=True), None, [txt], queue=False)
 
-    demo.queue().launch(server_port=port)
+        btn.click(add_text, [chatbot, txt], [chatbot, txt], queue=False).then(
+            bot, [chatbot, state, allowed], [chatbot, state]
+        ).then(lambda: gr.update(interactive=True), None, [txt], queue=False).then(
+            lambda state: state['images'], [state], [gallery]
+        )
+        gallery.select(on_select_image, [state], [prompt])
+
+    demo.queue(concurrency_count=max_users).launch(server_port=port, share=share)
 
 
 if __name__ == "__main__":
